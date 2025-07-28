7a30966a88fe9939dc3beb0000753f6e740757f7
Marko Kljakovic Gaspic <marko.gaspic@capture.hr>
2025-07-28 07:11:57 +0200

    Main activity

---
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+}
